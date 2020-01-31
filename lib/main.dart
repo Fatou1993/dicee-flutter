37cc87d1814737547703cab39b5361be8a037e36
Ndeye Fatou Diop <nfdiop@palantir.com>
2020-01-31 07:02:22 +0100

    push changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber = 1;
+  int _rightDiceNumber = 1;
+  Random rndGenerator = Random();
+
+  void setRandomDiceNumbers(){
+    setState(() {
+      _leftDiceNumber = 1 + rndGenerator.nextInt(6);
+      _rightDiceNumber = 1 + rndGenerator.nextInt(6);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: setRandomDiceNumbers,
+                child: Image.asset('images/dice$_leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: setRandomDiceNumbers,
+                child: Image.asset('images/dice$_rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
